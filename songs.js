47698b5e808888a9b9fde93a92b67219dd65f0f2
Elie Schoppik <eschoppik@gmail.com>
2016-03-07 11:29:03 -0800

    removing old ids

--- songs.js
+++ songs.js
@@ -7,14 +7,14 @@ $(function() {
       randomSongId,
       neutralFace = "http://images.clipartpanda.com/neutral-face-black-and-white-yellow-neutral-face-1.jpg",
       sadFace = "http://upload.wikimedia.org/wikipedia/commons/e/e9/Sad_face.svg",
-      happyFace = "http://upload.wikimedia.org/wikipedia/commons/f/fc/Happy_face.svg", 
+      happyFace = "http://upload.wikimedia.org/wikipedia/commons/f/fc/Happy_face.svg",
       songIds = ["995535015", "966411602", "823593456", "956689796", "943946671",
                  "982388023", "907242704", "201281527", "656801339", "910038357",
                  "250038575", "878000348",  "794095205",  "1645339",  "400835962",
-                 "325618", "191924084",  "376116617",  "169003415",  "51958108",
-                 "76532142", "192688540", "684811768", "344799464", "217633921",
-                 "192811017", "258404365", "71068886", "640047583", "517438248",
-                 "266075552", "656479859", "310237", "991390352", "901614155",
+                 "325618",  "169003415",  "51958108",
+                 "192688540", "684811768", "344799464", "217633921",
+                 "192811017", "71068886", "640047583", "517438248",
+                "656479859", "310237", "991390352", "901614155",
                  "344799727", "162337613", "121695005", "159293848", "305118379" ];
 
 
@@ -22,7 +22,7 @@ $(function() {
   $("#startbutton").click(function(e) {
     playNewSong();
   });
-  
+
   $("#guessform").submit(function(e) {
     e.preventDefault();
 
@@ -57,7 +57,7 @@ $(function() {
       $answerStatus.removeClass("incorrecttext");
       $answerStatus.addClass("correcttext");
       $answerStatus.html("Correct!");
-      
+
     } else {
       wrongCount++;
       $answerStatus.removeClass("correcttext");
@@ -70,12 +70,12 @@ $(function() {
     $("#album").html(curSongData.collectionName);
     $("#coverart").attr("src", curSongData.artworkUrl100);
   }
-  
+
   function displayStats() {
     makeActive("guessstats");
     $("#audio_preview")[0].pause();
 
-    $("#correctguesses").html(correctCount); 
+    $("#correctguesses").html(correctCount);
     $("#wrongguesses").html(wrongCount);
     var srcUrl = "";
     if (correctCount > wrongCount) {
@@ -105,7 +105,7 @@ $(function() {
     if (results && results.length > 0) {
       return true;
     }
-   
+
     return false;
   }
 
@@ -118,13 +118,13 @@ $(function() {
       var first = data.results[0];
       var audioElem = $("#audio_preview");
       audioElem.attr("src", first.previewUrl);
-      curSongData = first; 
+      curSongData = first;
       audioElem.on('canplay', function() {
-        audioElem[0].play(); 
+        audioElem[0].play();
       });
     });
   }
-  
+
   function getAndSetRandomSongId(songs) {
     var randNum = Math.floor((Math.random() * songs.length));
     randomSongId = songs[randNum];
